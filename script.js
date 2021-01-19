16b0f38a7b1da29b8c031087db53cae2095e4654
Manav Deep Singh Lamba <56251839+techschneiderrr@users.noreply.github.com>
2021-01-19 12:25:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 const html = document.documentElement;
 const canvas = document.getElementById("hero-lightpass");
 const context = canvas.getContext("2d");
+
+const frameCount = 148;
+const currentFrame = index => (
+  `https://www.apple.com/105/media/us/airpods-pro/2019/1299e2f5_9206_4470_b28e_08307a42f19b/anim/sequence/large/01-hero-lightpass/${index.toString().padStart(4, '0')}.jpg`
+)
+
+const preloadImages = () => {
+  for (let i = 1; i < frameCount; i++) {
+    const img = new Image();
+    img.src = currentFrame(i);
+  }
+};
+
